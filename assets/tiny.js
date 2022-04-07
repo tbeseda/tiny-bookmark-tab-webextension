68c234c9c896e71dfffa5dd6971c889bee99005e
Taylor Beseda <tbeseda@gmail.com>
2022-04-07 12:40:27 -0600

    re-style

--- assets/tiny.js
+++ assets/tiny.js
@@ -1,6 +1,8 @@
 function List(bookmarks) {
   return `
-<ul>
+<ul
+  class="pl-2"
+>
   ${bookmarks
     .map(
       (bookmark) => `
@@ -19,14 +21,29 @@ function List(bookmarks) {
 
 function Item(bookmark) {
   return `
-<li><a class="link" href="${bookmark.url}">${bookmark.title}</a></li>
+<li
+  class="truncate ml-1 pl-2 border-l-2 border-slate-300 dark:border-gray-700"
+>
+  <a
+    class="p-1 text-blue-500 hover:bg-gray-200 dark:text-blue-300 dark:hover:bg-slate-700"
+    href="${bookmark.url}"
+  >
+    ${bookmark.title}
+  </a>
+</li>
   `.trim()
 }
 
 function Folder(bookmark) {
   return `
-<li>
-  <strong class="title">${bookmark.title} (${bookmark.children.length})</strong>
+<li
+  class="mt-2"
+>
+  <span
+    class="font-semibold text-gray-600 dark:text-gray-200"
+  >
+    ${bookmark.title} (${bookmark.children.length})
+  </span>
   ${List(bookmark.children)}
 </li>
   `.trim()
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["tiny.html", "./assets/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
