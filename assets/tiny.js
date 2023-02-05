9584cf5437ae6b56a27892a1be2e10b2650c5e3a
Taylor Beseda <tbeseda@gmail.com>
2023-02-05 14:39:27 -0700

    simple styling

--- assets/tiny.js
+++ assets/tiny.js
@@ -5,71 +5,65 @@ function Favicon(bookmark) {
 		const domain = url.hostname;
 		const imageUrl = `https://icons.duckduckgo.com/ip3/${domain}.ico`;
 
-		return `<img src="${imageUrl}" alt="${domain} favicon">`.trim();
+		return `<img height=20 width=20 src="${imageUrl}" alt="${domain} favicon">`.trim();
 	}
 
 	return "";
 }
 
 function List(bookmarks) {
-	console.table(bookmarks);
 	return `
 <ul>
-  ${bookmarks
+	${bookmarks
 		.map(
 			(bookmark) => `
-        ${
+				${
 					typeof bookmark.children === "undefined"
 						? Item(bookmark)
 						: Folder(bookmark)
 				}
-      `,
+			`,
 		)
 		.join("")}
-</ul>
-  `.trim();
+</ul>`;
 }
 
 function Item(bookmark) {
 	return `
 <li>
-  <a href="${bookmark.url}">
-    ${Favicon(bookmark)}
-    <span>${bookmark.title}</span>
-  </a>
-</li>
-  `.trim();
+	<a href="${bookmark.url}">
+		${Favicon(bookmark)}
+		<span>${bookmark.title}</span>
+	</a>
+</li>`;
 }
 
 function Folder(bookmark) {
 	return `
 <li>
-  <span>
-    ${bookmark.title} (${bookmark.children.length})
-  </span>
-  ${List(bookmark.children)}
-</li>
-  `.trim();
+	<span>
+		${bookmark.title} (${bookmark.children.length})
+	</span>
+	${List(bookmark.children)}
+</li>`;
 }
 
 /** @param {import('webextension-polyfill').Browser} browser */
 async function main(browser) {
 	const $main = document.getElementById("main");
 	if (!$main) throw new Error("No #main element found");
+
 	$main.innerHTML = "Loading...";
+
 	const tree = await browser.bookmarks.getTree();
-	$main.innerHTML = List(tree[0].children);
+
+	$main.innerHTML = List(tree[0].children?.reverse());
 }
 
-console.log("booting");
-
 document.addEventListener("DOMContentLoaded", function () {
-	// TODO: bifurcate for Firefox vs Chrome
-	if (typeof browser === "undefined") {
-		// Chrome
+	if (typeof chrome !== "undefined") {
 		main(chrome);
-	} else {
-		// Firefox
-		main(browser);
+	} else if (typeof browser !== "undefined") {
+		main(browser); // Firefox
 	}
 });
